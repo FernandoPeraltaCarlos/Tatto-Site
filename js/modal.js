a451cf3d71d07670cbf61e664b699100fd70f964
Fernando Peralta <skate_tck08@hotmail.com>
2021-09-21 02:31:22 -0500

    se modifico el efecto dentro los testimonios(fade), se crearon los apartados para las imagenes de los trabajos con su efecto hover

---
+++ js/modal.js
@@ -0,0 +1,10 @@
+Swal.fire({
+    width:'60%',
+    height:'60%',
+    background: 'rgba(255,255,255,0)',
+    imageUrl: './../img/tatto-ofert.jpg',
+    imageWidth: 700,
+    imageHeight: 500,
+    imageAlt: 'Custom image',
+    showConfirmButton:false
+  });
--- js/testimonial.js
+++ js/testimonial.js
@@ -6,8 +6,12 @@ function Slide(){
     if(a>4){
         a=0;
     }
-    padre.eq(a-1).css("display","none");
-    padre.eq(a).css("display","flex");
-    console.log(a);
+    /*padre.eq(a-1).css("display","none");
+    padre.eq(a).css("display","flex");*/
+    padre.eq(a-1).fadeOut(1000);
+    setTimeout(function(){
+        padre.eq(a).fadeIn(1000);
+        padre.eq(a).css("display","flex");
+    },1000);
 }
 setInterval(Slide,6000,a);
