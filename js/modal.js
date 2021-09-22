f0d65811f464c8711fa89f1ba53154d6392bbe53
Fernando Peralta <skate_tck08@hotmail.com>
2021-09-21 19:46:23 -0500

    modificacion de enlaces para github

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,7 @@ Swal.fire({
     width:'60%',
     height:'60%',
     background: 'rgba(255,255,255,0)',
-    imageUrl: '../img/tatto-ofert.jpg',
+    imageUrl: 'img/tatto-ofert.jpg',
     imageWidth: 700,
     imageHeight: 500,
     imageAlt: 'Custom image',
